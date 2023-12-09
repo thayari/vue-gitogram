ff5df31a9ab2c92dc54f94a8323f2d61aa7a39fb
thayari <50718467+thayari@users.noreply.github.com>
2023-12-10 01:20:19 +0300

    Create LoaderView.vue

---
+++ src/components/LoaderView/LoaderView.vue
@@ -0,0 +1,28 @@
+<template>
+<span class="icon">
+  <IconView :name="IconLoading"/>
+</span>
+</template>
+
+<script>
+import IconView from '@/icons/IconView'
+export default {
+  name: 'LoaderView',
+  components: { IconView }
+}
+</script>
+
+<style scoped>
+.icon {
+  animation: rotation 3s linear;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
